9cb774ef9032c874950c834a598c7627d97f22c9
Julius-central <julio.david.velasquez@gmail.com>
2021-09-01 21:39:09 -0600

    ADD - linear regresion

--- Caso.R
+++ Caso.R
@@ -82,7 +82,7 @@ by_country %>%
   filter(total > 100)
 
 # ----------------------------------------------------------------------
-# GRAFICAS
+# GRAPHS
 # ----------------------------------------------------------------------
 
 # Define by_year
@@ -97,4 +97,98 @@ library(ggplot2)
 # Create line plot
 ggplot(by_year, aes(x = year, y = percent_yes)) +
   geom_line()
+
+# Change to scatter plot and add smoothing curve
+ggplot(by_year, aes(year, percent_yes)) +
+  geom_smooth() +
+  geom_point()
+
+
+# ----------------------------------------------------------------------
+# COUNTRY
+# ----------------------------------------------------------------------
+
+# Group by year and country: by_year_country
+by_year_country <- votes_processed %>%
+  group_by(year, country) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1))
+
+
+# Print by_year_country
+by_year_country
+
+# Create a filtered version: UK_by_year
+UK_by_year <- by_year_country %>%
+  filter(country == "United Kingdom")
+
+# Line plot of percent_yes over time for UK only
+ggplot(UK_by_year, aes(x= year, y = percent_yes)) +
+  geom_line()
+
+# Vector of four countries to examine
+countries <- c("United States", "United Kingdom",
+               "France", "India")
+
+# Filter by_year_country: filtered_4_countries
+filtered_4_countries<- by_year_country %>% 
+  filter(country %in% countries)
+
+# Line plot of % yes in four countries
+ggplot(filtered_4_countries, aes(x= year ,y=percent_yes, color = country)) +
+  geom_line()
+
+# ----------------------------------------------------------------------
+# FACET WRAP
+# ----------------------------------------------------------------------
+
+# Vector of six countries to examine
+countries <- c("United States", "United Kingdom",
+               "France", "Japan", "Brazil", "India")
+
+# Filtered by_year_country: filtered_6_countries
+filtered_6_countries <- by_year_country %>%
+  filter(country %in% countries)
+
+# Line plot of % yes over time faceted by country
+ggplot(filtered_6_countries, aes(x=year,y =percent_yes)) +
+  geom_line() + 
+  facet_wrap(~ country)
+
+# Line plot of % yes over time faceted by country
+ggplot(filtered_6_countries, aes(year, percent_yes)) +
+  geom_line() +
+  facet_wrap(~ country, scales = "free_y")
+
+
+
+# ----------------------------------------------------------------------
+# LINEAR REGRESSION
+# ----------------------------------------------------------------------
+
+# Percentage of yes votes from the US by year: US_by_year
+US_by_year <- by_year_country %>%
+  filter(country == "United States")
+
+# Print the US_by_year data
+US_by_year
+
+# Perform a linear regression of percent_yes by year: US_fit
+US_fit <- lm(percent_yes ~ year, data = US_by_year)
+
+# Perform summary() on the US_fit object
+summary(US_fit)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
